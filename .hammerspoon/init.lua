00146bc1c97dd5fb02e1f65aa6d854873b634807
Gjermund Garaba <gjermund@garaba.net>
2025-05-22 19:41:59 +0200

    Update hammerspoon shortcuts

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,34 @@
+-- hs.loadSpoon("Vifari")
+-- spoon.Vifari:start() -- this will add hooks. `:stop()` to remove hooks
+
+hs.hotkey.bind({}, "F1", function()
+	hs.application.launchOrFocus("Ghostty")
+end)
+
+hs.hotkey.bind({}, "F2", function()
+	hs.application.launchOrFocus("Zen Browser")
+end)
+
+hs.hotkey.bind({}, "F3", function()
+	hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bind({}, "F4", function()
+	hs.application.launchOrFocus("Linear")
+end)
+
+hs.hotkey.bind({}, "F5", function()
+	hs.application.launchOrFocus("Notion")
+end)
+
+hs.hotkey.bind({}, "F6", function()
+	hs.application.launchOrFocus("ChatGPT")
+end)
+
+hs.hotkey.bind({}, "F9", function()
+	hs.application.launchOrFocus("Messages")
+end)
+
+hs.hotkey.bind({}, "F10", function()
+	hs.application.launchOrFocus("Spotify")
+end)
