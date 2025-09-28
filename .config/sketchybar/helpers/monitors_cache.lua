6f6d7c054819f6bbe857b847a00b56269f9ced7b
Gjermund Garaba <gjermund@garaba.net>
2025-09-28 23:45:51 +0300

    remove sketchybar for now

--- .config/sketchybar/helpers/monitors_cache.lua
+++
@@ -1,8 +0,0 @@
--- Simple module to share dynamically-resolved monitor mapping across requires
--- init.lua populates M.map before requiring modules that depend on settings.lua
-local M = {
-  map = nil,
-}
-
-return M
-
